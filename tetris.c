789524b3626ec4f9d1498a2ec7e47aa4f0ce0549
Carlos Israel Orta Orta <carlos.orta@coreapps.mx>
2024-04-24 08:22:52 -0600

    Use stack memory only and few optimizations, added colors to shapes, anticlockwise rotation and Added functionlity of hard-drop, hold and next 6 pieces

--- tetris.c
+++ tetris.c
@@ -5,66 +5,86 @@
 #include <ncurses.h>
 
 #define ROWS 20 // you can change height and width of table with ROWS and COLS 
-#define COLS 15
+#define COLS 10
 #define TRUE 1
 #define FALSE 0
+#define MAXSHAPES 7
+#define MAXNEXTPIECES 6
 
-char Table[ROWS][COLS] = {0};
+char table[ROWS][COLS] = {0};
+char *Table[ROWS] = {0};
 int score = 0;
 char GameOn = TRUE;
 suseconds_t timer = 400000; // decrease this to make it faster
 int decrease = 1000;
+int newLines = 0;
 
 typedef struct {
-    char **array;
+    char array[4][4];
+    char wasHold;
     int width, row, col;
 } Shape;
-Shape current;
+Shape current, pieceHold, *hold = NULL;
 
-const Shape ShapesArray[7]= {
-	{(char *[]){(char []){0,1,1},(char []){1,1,0}, (char []){0,0,0}}, 3},                           //S shape     
-	{(char *[]){(char []){1,1,0},(char []){0,1,1}, (char []){0,0,0}}, 3},                           //Z shape     
-	{(char *[]){(char []){0,1,0},(char []){1,1,1}, (char []){0,0,0}}, 3},                           //T shape     
-	{(char *[]){(char []){0,0,1},(char []){1,1,1}, (char []){0,0,0}}, 3},                           //L shape     
-	{(char *[]){(char []){1,0,0},(char []){1,1,1}, (char []){0,0,0}}, 3},                           //flipped L shape    
-	{(char *[]){(char []){1,1},(char []){1,1}}, 2},                                                 //square shape
-	{(char *[]){(char []){0,0,0,0}, (char []){1,1,1,1}, (char []){0,0,0,0}, (char []){0,0,0,0}}, 4} //long bar shape
+Shape nextPiece[MAXNEXTPIECES];
+int iNextPiece = 0, jNextPiece = MAXNEXTPIECES - 1;
+
+Shape ShapesArray[MAXSHAPES]= {
+	{{{0,'G','G',0},{'G','G',0,0}, {0,0,0,0}, {0,0,0,0}}, 0, 3}, //S shape     
+	{{{'R','R',0,0},{0,'R','R',0}, {0,0,0,0}, {0,0,0,0}}, 0, 3}, //Z shape     
+	{{{0,'P',0,0},{'P','P','P',0}, {0,0,0,0}, {0,0,0,0}}, 0, 3}, //T shape     
+	{{{0,0,'O',0},{'O','O','O',0}, {0,0,0,0}, {0,0,0,0}}, 0, 3}, //L shape     
+	{{{'B',0,0,0},{'B','B','B',0}, {0,0,0,0}, {0,0,0,0}}, 0, 3}, //flipped L shape    
+	{{{'Y','Y',0,0},{'Y','Y',0,0}, {0,0,0,0}, {0,0,0,0}}, 0, 2}, //square shape
+//	{{{0,1,0,0},{0,1,0,0}, {0,1,0,0}, {1,1,1,0}}, 'P', 4}, //long T shape     
+	{{{0,0,0,0},{'C','C','C','C'}, {0,0,0,0}, {0,0,0,0}}, 0, 4}  //long bar shape
 	// you can add any shape like it's done above. Don't be naughty.
 };
 
+char CopyMemShape(Shape *dst, Shape *org){
+    int i;
+	long long *orgL = (long long*)org, *dstL = (long long*)dst;
+	if (dst == NULL || org == NULL)
+		return 0;
+	for (i = 0; i < sizeof(Shape) / sizeof(long long); i++)
+		dstL[i] = orgL[i];
+	i = i * sizeof(long long);
+	for (i = i; i < sizeof(Shape); i++)
+		dst[i] = org[i];
+	return 1;
+}
+
 Shape CopyShape(Shape shape){
-	Shape new_shape = shape;
-	char **copyshape = shape.array;
-	new_shape.array = (char**)malloc(new_shape.width*sizeof(char*));
-    int i, j;
-    for(i = 0; i < new_shape.width; i++){
-		new_shape.array[i] = (char*)malloc(new_shape.width*sizeof(char));
-		for(j=0; j < new_shape.width; j++) {
-			new_shape.array[i][j] = copyshape[i][j];
-		}
-    }
+	Shape new_shape = {0};
+    int i;
+	char *org = (char*)&shape, *dst = (char*)&new_shape;
+	long long *orgL = (long long*)org, *dstL = (long long*)dst;
+	for (i = 0; i < sizeof(Shape) / sizeof(long long); i++)
+		dstL[i] = orgL[i];
+	i = i * sizeof(long long);
+	for (i = i; i < sizeof(Shape); i++)
+		dst[i] = org[i];
     return new_shape;
 }
 
 void DeleteShape(Shape shape){
     int i;
     for(i = 0; i < shape.width; i++){
-		free(shape.array[i]);
+//		free(shape.array[i]);
     }
-    free(shape.array);
+//    free(shape.array);
 }
 
 int CheckPosition(Shape shape){ //Check the position of the copied shape
-	char **array = shape.array;
 	int i, j;
 	for(i = 0; i < shape.width;i++) {
 		for(j = 0; j < shape.width ;j++){
 			if((shape.col+j < 0 || shape.col+j >= COLS || shape.row+i >= ROWS)){ //Out of borders
-				if(array[i][j]) //but is it just a phantom?
+				if(shape.array[i][j]) //but is it just a phantom?
 					return FALSE;
 				
 			}
-			else if(Table[shape.row+i][shape.col+j] && array[i][j])
+			else if(Table[shape.row+i][shape.col+j] && shape.array[i][j])
 				return FALSE;
 		}
 	}
@@ -72,27 +92,30 @@ int CheckPosition(Shape shape){ //Check the position of the copied shape
 }
 
 void SetNewRandomShape(){ //updates [current] with new shape
-	Shape new_shape = CopyShape(ShapesArray[rand()%7]);
-
+	Shape new_shape = nextPiece[iNextPiece];
     new_shape.col = rand()%(COLS-new_shape.width+1);
     new_shape.row = 0;
-    DeleteShape(current);
+//    DeleteShape(current);
 	current = new_shape;
 	if(!CheckPosition(current)){
 		GameOn = FALSE;
 	}
+	iNextPiece = (iNextPiece + 1) % MAXNEXTPIECES == 0 ? 0 : iNextPiece + 1;
+	jNextPiece = (jNextPiece + 1) % MAXNEXTPIECES == 0 ? 0 : jNextPiece + 1;
+	CopyMemShape(&nextPiece[jNextPiece],&ShapesArray[rand()%MAXSHAPES]);
 }
 
-void RotateShape(Shape shape){ //rotates clockwise
-	Shape temp = CopyShape(shape);
+void RotateShape(Shape *shape, char rotation){ //rotates clockwise
+	Shape temp = CopyShape(*shape);
 	int i, j, k, width;
-	width = shape.width;
-	for(i = 0; i < width ; i++){
-		for(j = 0, k = width-1; j < width ; j++, k--){
-				shape.array[i][j] = temp.array[k][i];
-		}
-	}
-	DeleteShape(temp);
+	width = shape->width;
+	for(i = 0; i < width ; i++)
+		for(j = 0, k = width-1; j < width ; j++, k--)
+			if (rotation == 'r')
+				shape->array[i][j] = temp.array[k][i];
+			else
+				shape->array[k][i] = temp.array[i][j];
+//	DeleteShape(temp);
 }
 
 void WriteToTable(){
@@ -110,20 +133,45 @@ void RemoveFullRowsAndUpdateScore(){
 	for(i=0;i<ROWS;i++){
 		sum = 0;
 		for(j=0;j< COLS;j++) {
-			sum+=Table[i][j];
+			if (Table[i][j])
+				sum += 1;
 		}
 		if(sum==COLS){
 			count++;
 			int l, k;
-			for(k = i;k >=1;k--)
-				for(l=0;l<COLS;l++)
-					Table[k][l]=Table[k-1][l];
-			for(l=0;l<COLS;l++)
-				Table[k][l]=0;
+			char *cleanRow = Table[i];
+			long long *erase = (long long*)cleanRow;
+			for (k = i; k >= 1; k--)
+				Table[k] = Table[k-1];
+			Table[0] = cleanRow;
+			for (k = 0; k < COLS/sizeof(long long); k++)
+				erase[k] = 0;
+			k = k * sizeof(long long);
+			for (k = k; k < COLS; k++)
+				cleanRow[k] = 0;
 			timer-=decrease--;
 		}
 	}
 	score += 100*count;
+	if (count > 1){
+		newLines -= count; 
+	}
+}
+
+void AddPendingLines(){
+	Shape temp = CopyShape(current);
+	temp.row++;
+	int i, j;
+	while (CheckPosition(temp) && newLines > 0){
+		char *incrementLine = Table[0];
+		j = rand() % COLS;
+		for (i = 0; i < COLS; i++)
+			incrementLine[i] = i == j ? 0 : '#';
+		for (i = 1; i < ROWS; i++)
+			Table[i - 1] = Table[i];
+		Table[ROWS - 1] = incrementLine;
+		newLines--;
+	}
 }
 
 void PrintTable(){
@@ -138,10 +186,16 @@ void PrintTable(){
 	clear();
 	for(i=0; i<COLS-9; i++)
 		printw(" ");
-	printw("Covid Tetris\n");
+	printw("Covid Tetris NEXT: ");
+	j = iNextPiece;
+	for (i = 0; i < MAXNEXTPIECES; i++){
+		printw("%c -> ", nextPiece[j].array[1][1]);
+		j = (j + 1) % MAXNEXTPIECES == 0 ? 0 : j + 1;
+	}
+	printw(" HOLD: %c \n", hold == NULL ? ' ' : hold->array[1][1]);
 	for(i = 0; i < ROWS ;i++){
 		for(j = 0; j < COLS ; j++){
-			printw("%c ", (Table[i][j] + Buffer[i][j])? '#': '.');
+			printw("%c ", (Table[i][j] + Buffer[i][j])? (Buffer[i][j]?Buffer[i][j]:Table[i][j]): '.');
 		}
 		printw("\n");
 	}
@@ -151,7 +205,21 @@ void PrintTable(){
 void ManipulateCurrent(int action){
 	Shape temp = CopyShape(current);
 	switch(action){
+		case 'w':
+			if (newLines < 0)
+				newLines = 0;
+			temp.row++;  //move down
+			while (CheckPosition(temp))
+				temp.row++;
+			current.row = temp.row - 1;
+			WriteToTable();
+			RemoveFullRowsAndUpdateScore();
+            SetNewRandomShape();
+			AddPendingLines();
+			break;
 		case 's':
+			if (newLines < 0)
+				newLines = 0;
 			temp.row++;  //move down
 			if(CheckPosition(temp))
 				current.row++;
@@ -159,6 +227,7 @@ void ManipulateCurrent(int action){
 				WriteToTable();
 				RemoveFullRowsAndUpdateScore();
                 SetNewRandomShape();
+				AddPendingLines();
 			}
 			break;
 		case 'd':
@@ -171,13 +240,39 @@ void ManipulateCurrent(int action){
 			if(CheckPosition(temp))
 				current.col--;
 			break;
-		case 'w':
-			RotateShape(temp); // rotate clockwise
+		case 'r':
+			if (current.wasHold)
+				break;
+			if (hold == NULL){
+				hold = &pieceHold;
+				CopyMemShape(hold,&current);
+				SetNewRandomShape();
+				current.wasHold = 1;
+			} else{
+				CopyMemShape(&temp,&current);
+				CopyMemShape(&current,hold);
+				CopyMemShape(hold,&temp);
+				current.col = rand()%(COLS-current.width+1);
+				current.row = 0;
+				current.wasHold = 1;
+//			    DeleteShape(current);
+				if(!CheckPosition(current)){
+					GameOn = FALSE;
+				}
+			}
+			break;
+		case 'e':
+			RotateShape(&temp, 'r'); // rotate clockwise
 			if(CheckPosition(temp))
-				RotateShape(current);
+				RotateShape(&current,'r');
+			break;
+		case 'q':
+			RotateShape(&temp, 'l'); // rotate anticlockwise
+			if(CheckPosition(temp))
+				RotateShape(&current,'l');
 			break;
 	}
-	DeleteShape(temp);
+//	DeleteShape(temp);
 	PrintTable();
 }
 
@@ -188,6 +283,12 @@ int hasToUpdate(){
 
 int main() {
     srand(time(0));
+	for (int i = 0; i < ROWS; i++)
+		Table[i] = table[i];
+	for (int i = 0; i < MAXNEXTPIECES; i++){
+		Shape new_shape = CopyShape(ShapesArray[rand()%MAXSHAPES]);
+		CopyMemShape(&nextPiece[i], &new_shape);
+	}
     score = 0;
     int c;
     initscr();
@@ -195,6 +296,7 @@ int main() {
 	timeout(1);
 	SetNewRandomShape();
     PrintTable();
+	int wait = 5;
 	while(GameOn){
 		if ((c = getch()) != ERR) {
 		  ManipulateCurrent(c);
@@ -203,6 +305,10 @@ int main() {
 		if (hasToUpdate()) { //time difference in microsec accuracy
 			ManipulateCurrent('s');
 			gettimeofday(&before_now, NULL);
+			if (wait == 0){
+				wait = 5;
+				newLines += 1;
+			} else wait--;
 		}
 	}
 	DeleteShape(current);
